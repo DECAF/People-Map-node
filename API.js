5003ba80a1c33f0e67b6abe8320a338f87c181e0
grmlin <shopping@hardwehr.de>
2013-02-20 18:06:49 +0100

    first commit

---
+++ API.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MAP_ENTRIES   : '/api/entries',
+  API_SAVE_JSON : '/api/entries/save/json'
+};
--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var path = require('path'),
   express = require('express'),
-  app = express();
+  app = express(),
+  API = require('./API'),
+  defaultEntry = require('./editor-defaults.json');
 
-var PEOPLE_MAP_DIST_DIR = path.join(__dirname, '../', 'People-Map', 'dist'); 
+var PEOPLE_MAP_DIST_DIR = path.join(__dirname, '../', 'People-Map', 'dist');
 
 app.use(express.static(PEOPLE_MAP_DIST_DIR));
+app.use(express.bodyParser());
+
+app.get(API.MAP_ENTRIES, function (req, res) {
+  res.json(defaultEntry);
+});
+
+app.get(API.API_SAVE_JSON + '/:data', function (req, res) {
+  try {
+    var data = decodeURIComponent(req.params.data);
+    if (data) {
+      res.setHeader('Content-Type', 'application/json;');
+      res.setHeader('Content-disposition', 'attachment; filename=map-entry.json');
+      res.json(JSON.parse(data));
+    }
+  } catch (e) {
+    res.send(500, {
+      error : "Oops, Saving your json failed. \n #{e}"
+    })
+  }
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
